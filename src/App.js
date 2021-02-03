29f5a24149f1650eb841db60be85d9f58b9d5a42
taurusilver7 <prasad20697@gmail.com>
2021-02-03 19:06:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Parallax } from "react-parallax";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
+import Container from "react-bootstrap/Container";
 
-function App() {
+import MyNavbar from "./components/Navbar/MyNavbar";
+import MyCarousel from "./components/Carousel/MyCarousel";
+import Titlegreeting from "./components/TitleGreeting/Titlegreeting";
+import MyTimeline from "./components/Timeline/Timeline";
+import Footer from "./components/Footer/Footer";
+
+import About from "./pages/About/About";
+import Skills from "./pages/Skills/Skills";
+import Contact from "./pages/Contact/Contact";
+
+import Particles from "react-particles-js";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app" style={{ position: "relative" }}>
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 40,
+              density: {
+                enable: true,
+                value_area: 1200,
+              },
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          },
+        }}
+      />
+      <MyCarousel />
+      <Titlegreeting />
+      <MyNavbar />
+
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/background/background.webp")}
+          bgImageAlt=""
+          strength={-200}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
+
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <MyTimeline />
+        </Slide>
+      </Container>
+
+      <Container className="container-box rounded">
+        <Fade duration={500}>
+          <hr />
+          <Contact />
+        </Fade>
+      </Container>
+
+      <hr />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
